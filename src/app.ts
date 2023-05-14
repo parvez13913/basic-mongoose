dbe618d91494ae4ce2daaab34598733c82db3773
parvez13913 <parvezz13913@gmail.com>
2023-05-14 12:52:45 +0600

    Install and implement type

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+
+const app: Application = express();
+// Using cors
+app.use(cors());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+  next();
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+import mongoose from "mongoose";
+import app from "./app";
+
 const port = 5000;
 
 // Database Connection
-
 async function bootstrap() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/Practice-mongoose");
     console.log("Database Connection Successfull");
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
   } catch (err) {
     console.log("Fail To connect Database", err);
   }
 }
 
 bootstrap();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
