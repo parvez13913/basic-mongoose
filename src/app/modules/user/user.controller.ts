7ed79d50ef6352a92e8484893aa221f6af526d8c
parvez13913 <parvezz13913@gmail.com>
2023-05-15 18:34:14 +0600

    field filter

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDb, getUsersFromDb } from "./user.service";
+import {
+  createUserToDb,
+  getUserByIdFromDb,
+  getUsersFromDb,
+} from "./user.service";
 
 export const createUser = async (
   req: Request,
@@ -23,6 +27,19 @@ export const getUsers = async (
   res.status(200).json({
     status: "success",
     data: users,
+  });
+};
+
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const user = await getUserByIdFromDb(id);
+  res.status(200).json({
+    status: "success",
+    data: user,
   });
 };
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -11,3 +11,10 @@ export const getUsersFromDb = async (): Promise<IUser[]> => {
   const users = await User.find();
   return users;
 };
+
+export const getUserByIdFromDb = async (
+  payload: string
+): Promise<IUser | null> => {
+  const user = await User.findOne({ id: payload }, { name: 1 });
+  return user;
+};
